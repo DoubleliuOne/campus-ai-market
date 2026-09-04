47ea56d3d38289779200da83d94af417b776028c
Doubleliu <Doubleliur@gmail>
2026-09-04 16:37:01 +0800

    chore: init Spring Boot 3.5.4 project

---
+++ src/main/java/com/campusmarket/CampusaiMarketApplication.java
@@ -0,0 +1,12 @@
+package com.campusmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusaiMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusaiMarketApplication.class, args);
+    }
+}
